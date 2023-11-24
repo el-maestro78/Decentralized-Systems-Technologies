a7bf6c606d56c832598398de2db67bdb10fea994
manosmin <manosmin@hotmail.com>
2023-11-24 02:41:06 +0200

    now replicates rows and splits educations

--- chord_new/csv_to_dict.py
+++ chord_new/csv_to_dict.py
@@ -7,16 +7,18 @@ def create_education_dictionary(csv_file):
     with open(csv_file, 'r', encoding="utf-8") as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            education_str = row['Education'].split(',')[0].strip('[]').replace("'", "").replace("\"", "")  # Keeps only the first university if education has more than one and removes brackets and excess quotes
+            education_values = eval(row['Education'])
+            awards = int(row['#Awards'])
             surname = row['Surname']
-            awards = row['#Awards']
 
-            # If the education key is not in the dictionary, add it
-            if education_str not in education_dict:
-                education_dict[education_str] = [(surname, int(awards))]
-            else:
-                # If it already exists, add it to the existing list
-                education_dict[education_str].append((surname, awards))
+            for education_value in education_values:
+                education_str = education_value.strip("'\"")  # Remove excess quotes
+                # If the education key is not in the dictionary, add it
+                if education_str not in education_dict:
+                    education_dict[education_str] = [(surname, awards)]
+                else:
+                    # If it already exists, add it to the existing list
+                    education_dict[education_str].append((surname, awards))
 
     # Remove scientists with an empty education
     education_dict.pop('', None)
@@ -33,7 +35,7 @@ if __name__ == '__main__':
     # Κρατάει μόνο τα n πρώτα στοιχεία του dictionary 
     n = 25
     education_dictionary = dict(itertools.islice(education_dictionary.items(), n))
-
+    print(education_dictionary)
     for key, value in education_dictionary.items():
         print(f"Education: {key}")
         print(f"Scientists/Awards: {value}")
